ed2456dd941a2108eafe3804d960e4d8606a33a6
daniel-fudge <daniel.fudge1977@gmail.com>
2020-05-04 20:27:50 -0400

    Added click functionality

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Demo repo for experimenting with boto3
 ## About
 
 This project is a demo, and uses boto3 to manage AWS EC2 instance snapshots.
+
+## Configuring
+
+shotty.py uses the configuration file created by the AWS cli. e.g..
+
+`aws configure --profile shotty`
+
+## Running
+
+`pipenv run "python shotty/shotty.py <command> <--project=PROJECT>"`
+
+*command* is `list`, `start` ot `stop`
+*project* is optional
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,14 +5,58 @@ import click
 session = boto3.Session(profile_name='acg-demo-boto3')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+def filter_instances(project):
+    instances = list()
+
+    if project:
+        filters = [{'Name': 'tag:project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+
+@click.group()
+def instances():
+    """Commands for instances."""
+
+
+@instances.command('list')
+@click.option('--project', default=None,
+              help='Only instances for project (tag project:<name>)')
+def list_instances(project):
     """List EC2 instances."""
-    for i in ec2.instances.all():
+    for i in filter_instances(project):
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(', '.join((i.id, i.instance_type,
                          i.placement['AvailabilityZone'], i.state['Name'],
-                         i.public_dns_name)))
+                         i.public_dns_name,
+                         tags.get('project', '<no project>'))))
     return
 
+
+@instances.command('stop')
+@click.option('--project', default=None,
+              help='Only instances for project (tag project:<name>)')
+def stop_instances(project):
+    """Stop EC2 instances."""
+    for i in filter_instances(project):
+        print("Stopping {}...".format(i.id))
+        i.stop()
+    return
+
+
+@instances.command('start')
+@click.option('--project', default=None,
+              help='Only instances for project (tag project:<name>)')
+def start_instances(project):
+    """Start EC2 instances."""
+    for i in filter_instances(project):
+        print("Starting {}...".format(i.id))
+        i.start()
+    return
+
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
